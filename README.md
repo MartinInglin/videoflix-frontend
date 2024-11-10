b32393bb223545a528fad422eae0340ed6dbe60e
Zuegelwagen <m.inglin@gmx.ch>
2024-11-10 18:49:49 +0100

    readme added

--- README.md
+++ README.md
@@ -1,26 +1,100 @@
-# VideoflixFrontend
+# Project overview
 
+This frontend provides a user interface to play videos, registrate, login, logout, send verification emails and password reset
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.1.3.
+
+
+## System Requirements
+
+- Node.js (v16 or later)
+- Angular CLI (v17.1.3)
+- A compatible browser (latest version of Chrome, Firefox, etc.)
+
+
+## Installation
+
+1. Clone the repository:
+
+```bash
+git clone https://github.com/MartinInglin/videoflix-frontend
+cd videoflix-frontend
+npm install
+```
+
+
+## Project Structure
+
+- `src/app/features/auth/` - Contains all UI components for authentication.
+- `src/app/features/features/` - Contains all UI components for video display.
+- `src/app/interfaces/` - Contains interfaces.
+- `src/app/models/` - Contains models.
+- `src/app/services/` - Contains services for interacting with APIs.
+- `src/app/services/guards` - Contains the route guards for routes.ts.
+- `src/app/shared` - Contains all shared UI components, e.g. header.
+- `src/app/static` - Contains all static UI components, e.g. the imprint.
+- `src/app/utils` - Contains utlity functions.
+- `src/environments/` - Contains environment configuration files.
+
 
 ## Development server
 
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The application will automatically reload if you change any of the source files.
 
+
 ## Code scaffolding
 
 Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
+
+
+## Usage
+
+- **Playing Videos**: Navigate to the video library and select a video to start streaming.
+- **User Registration**: Create a new account by clicking on "Sign up" and filling in the required fields.
+- **Email verification**: Click on the link in the email and you will be directed to the verification page which verifies the email address automatically.
+- **Password Reset**: Click on "Forgot Password" to initiate the password reset process. An email will be sent which directs the user to the password reset page.
+- **Login**: Login with email and password to receive a token for authentication.
+
+
+## Environment variables
+
+Configure the environment variables in `src/environments/environment.ts`:
+
+```typescript
+export const environment = {
+  production: false,
+  apiUrl: 'http://'  // Set the backend API URL here
+};
+```
+
+## API Integration
+
+This frontend communicates with a backend API for user authentication, video streaming, and other features.
+Ensure that the backend server is running at the URL specified in `environment.ts` to connect successfully.
+
 
 ## Build
 
 Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory.
 
+
 ## Running unit tests
 
 Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
 
+
 ## Running end-to-end tests
 
 Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To use this command, you need to first add a package that implements end-to-end testing capabilities.
+
+
+## Deployment
+
+To deploy the frontend application, build the project and host the files in the `dist/` directory on your preferred platform.
+
+Example for building:
+```bash
+ng build --prod
+```
 
 ## Further help
 
---
+++ documentation/js/menu-wc.js
@@ -0,0 +1,211 @@
+'use strict';
+
+customElements.define('compodoc-menu', class extends HTMLElement {
+    constructor() {
+        super();
+        this.isNormalMode = this.getAttribute('mode') === 'normal';
+    }
+
+    connectedCallback() {
+        this.render(this.isNormalMode);
+    }
+
+    render(isNormalMode) {
+        let tp = lithtml.html(`
+        <nav>
+            <ul class="list">
+                <li class="title">
+                    <a href="index.html" data-type="index-link">videoflix-frontend documentation</a>
+                </li>
+
+                <li class="divider"></li>
+                ${ isNormalMode ? `<div id="book-search-input" role="search"><input type="text" placeholder="Type to search"></div>` : '' }
+                <li class="chapter">
+                    <a data-type="chapter-link" href="index.html"><span class="icon ion-ios-home"></span>Getting started</a>
+                    <ul class="links">
+                        <li class="link">
+                            <a href="overview.html" data-type="chapter-link">
+                                <span class="icon ion-ios-keypad"></span>Overview
+                            </a>
+                        </li>
+                        <li class="link">
+                            <a href="index.html" data-type="chapter-link">
+                                <span class="icon ion-ios-paper"></span>README
+                            </a>
+                        </li>
+                                <li class="link">
+                                    <a href="dependencies.html" data-type="chapter-link">
+                                        <span class="icon ion-ios-list"></span>Dependencies
+                                    </a>
+                                </li>
+                                <li class="link">
+                                    <a href="properties.html" data-type="chapter-link">
+                                        <span class="icon ion-ios-apps"></span>Properties
+                                    </a>
+                                </li>
+                    </ul>
+                </li>
+                    <li class="chapter">
+                        <div class="simple menu-toggler" data-bs-toggle="collapse" ${ isNormalMode ? 'data-bs-target="#components-links"' :
+                            'data-bs-target="#xs-components-links"' }>
+                            <span class="icon ion-md-cog"></span>
+                            <span>Components</span>
+                            <span class="icon ion-ios-arrow-down"></span>
+                        </div>
+                        <ul class="links collapse " ${ isNormalMode ? 'id="components-links"' : 'id="xs-components-links"' }>
+                            <li class="link">
+                                <a href="components/AppComponent.html" data-type="entity-link" >AppComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/DashboardComponent.html" data-type="entity-link" >DashboardComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/ErrorToastComponent.html" data-type="entity-link" >ErrorToastComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/ErrorToastCTAComponent.html" data-type="entity-link" >ErrorToastCTAComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/FooterComponent.html" data-type="entity-link" >FooterComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/ForgotPasswordComponent.html" data-type="entity-link" >ForgotPasswordComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/HeaderComponent.html" data-type="entity-link" >HeaderComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/HeroComponent.html" data-type="entity-link" >HeroComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/ImprintComponent.html" data-type="entity-link" >ImprintComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/LandingPageComponent.html" data-type="entity-link" >LandingPageComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/LoginComponent.html" data-type="entity-link" >LoginComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/PrivacyPolicyComponent.html" data-type="entity-link" >PrivacyPolicyComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/ResetPasswordComponent.html" data-type="entity-link" >ResetPasswordComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/SignUpComponent.html" data-type="entity-link" >SignUpComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/VerificationComponent.html" data-type="entity-link" >VerificationComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/VideoPlayerComponent.html" data-type="entity-link" >VideoPlayerComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/VjsPlayerComponent.html" data-type="entity-link" >VjsPlayerComponent</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="chapter">
+                        <div class="simple menu-toggler" data-bs-toggle="collapse" ${ isNormalMode ? 'data-bs-target="#classes-links"' :
+                            'data-bs-target="#xs-classes-links"' }>
+                            <span class="icon ion-ios-paper"></span>
+                            <span>Classes</span>
+                            <span class="icon ion-ios-arrow-down"></span>
+                        </div>
+                        <ul class="links collapse " ${ isNormalMode ? 'id="classes-links"' : 'id="xs-classes-links"' }>
+                            <li class="link">
+                                <a href="classes/DashboardVideoModel.html" data-type="entity-link" >DashboardVideoModel</a>
+                            </li>
+                            <li class="link">
+                                <a href="classes/HeroVideoModel.html" data-type="entity-link" >HeroVideoModel</a>
+                            </li>
+                            <li class="link">
+                                <a href="classes/Validation.html" data-type="entity-link" >Validation</a>
+                            </li>
+                            <li class="link">
+                                <a href="classes/VideoModel.html" data-type="entity-link" >VideoModel</a>
+                            </li>
+                        </ul>
+                    </li>
+                        <li class="chapter">
+                            <div class="simple menu-toggler" data-bs-toggle="collapse" ${ isNormalMode ? 'data-bs-target="#injectables-links"' :
+                                'data-bs-target="#xs-injectables-links"' }>
+                                <span class="icon ion-md-arrow-round-down"></span>
+                                <span>Injectables</span>
+                                <span class="icon ion-ios-arrow-down"></span>
+                            </div>
+                            <ul class="links collapse " ${ isNormalMode ? 'id="injectables-links"' : 'id="xs-injectables-links"' }>
+                                <li class="link">
+                                    <a href="injectables/AuthenticationService.html" data-type="entity-link" >AuthenticationService</a>
+                                </li>
+                                <li class="link">
+                                    <a href="injectables/AuthGuardService.html" data-type="entity-link" >AuthGuardService</a>
+                                </li>
+                                <li class="link">
+                                    <a href="injectables/AuthRedirectService.html" data-type="entity-link" >AuthRedirectService</a>
+                                </li>
+                                <li class="link">
+                                    <a href="injectables/LandingPageService.html" data-type="entity-link" >LandingPageService</a>
+                                </li>
+                                <li class="link">
+                                    <a href="injectables/ToastService.html" data-type="entity-link" >ToastService</a>
+                                </li>
+                                <li class="link">
+                                    <a href="injectables/VideoService.html" data-type="entity-link" >VideoService</a>
+                                </li>
+                            </ul>
+                        </li>
+                    <li class="chapter">
+                        <div class="simple menu-toggler" data-bs-toggle="collapse" ${ isNormalMode ? 'data-bs-target="#interfaces-links"' :
+                            'data-bs-target="#xs-interfaces-links"' }>
+                            <span class="icon ion-md-information-circle-outline"></span>
+                            <span>Interfaces</span>
+                            <span class="icon ion-ios-arrow-down"></span>
+                        </div>
+                        <ul class="links collapse " ${ isNormalMode ? ' id="interfaces-links"' : 'id="xs-interfaces-links"' }>
+                            <li class="link">
+                                <a href="interfaces/DashboardData.html" data-type="entity-link" >DashboardData</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/LoginResponse.html" data-type="entity-link" >LoginResponse</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/ToastCTA.html" data-type="entity-link" >ToastCTA</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="chapter">
+                        <div class="simple menu-toggler" data-bs-toggle="collapse" ${ isNormalMode ? 'data-bs-target="#miscellaneous-links"'
+                            : 'data-bs-target="#xs-miscellaneous-links"' }>
+                            <span class="icon ion-ios-cube"></span>
+                            <span>Miscellaneous</span>
+                            <span class="icon ion-ios-arrow-down"></span>
+                        </div>
+                        <ul class="links collapse " ${ isNormalMode ? 'id="miscellaneous-links"' : 'id="xs-miscellaneous-links"' }>
+                            <li class="link">
+                                <a href="miscellaneous/functions.html" data-type="entity-link">Functions</a>
+                            </li>
+                            <li class="link">
+                                <a href="miscellaneous/typealiases.html" data-type="entity-link">Type aliases</a>
+                            </li>
+                            <li class="link">
+                                <a href="miscellaneous/variables.html" data-type="entity-link">Variables</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="chapter">
+                        <a data-type="chapter-link" href="coverage.html"><span class="icon ion-ios-stats"></span>Documentation coverage</a>
+                    </li>
+                    <li class="divider"></li>
+                    <li class="copyright">
+                        Documentation generated using <a href="https://compodoc.app/" target="_blank" rel="noopener noreferrer">
+                            <img data-src="images/compodoc-vectorise.png" class="img-responsive" data-type="compodoc-logo">
+                        </a>
+                    </li>
+            </ul>
+        </nav>
+        `);
+        this.innerHTML = tp.strings;
+    }
+});
